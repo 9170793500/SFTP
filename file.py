31111898e102e8a322f434f61d775c1c690ae827
sundram <sundrammaurya1996@gmail.com>
2023-12-18 20:08:14 +0530

    new change

--- file.py
+++ file.py
@@ -1,56 +1,225 @@
-# import pysftp
+# import paramiko
 
+# # Server credentials
 # host = '112.196.76.14'
 # port = 9090
 # username = 'root'
 # password = 'dvnsv!1$6^5'
+# remote_file_path = '/home/PutAllFiveServerDndFile/MergedDndNoOfFiveServer_2023-12-18.xls'  
+# local_file_path = 'D:/SFTP/localfile.xls'  
+
+# # Create an SSH client
+# ssh_client = paramiko.SSHClient()
+
+
+# ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # try:
-#     conn = pysftp.Connection(host=host, port=port, username=username, password=password)
-#     print("Connection established successfully")
-    
-#     conn.close()
+#     # Connect to  server
+#     ssh_client.connect(hostname=host, port=port, username=username, password=password)
+
+#     # Create an SFTP client from the SSH client
+#     sftp_client = ssh_client.open_sftp()
+
+#     # Download the remote file
+#     sftp_client.get(remote_file_path, local_file_path)
+#     print(f"File downloaded successfully to {local_file_path}")
+
+#     # Close the SFTP session and the SSH connection
+#     sftp_client.close()
+#     ssh_client.close()
+
+# except paramiko.AuthenticationException:
+#     print("Authentication failed. Please check your credentials.")
+# except paramiko.SSHException as ssh_err:
+#     print(f"Unable to establish SSH connection: {ssh_err}")
 # except Exception as e:
-#     print(f'Failed to establish connection to the targeted server: {e}')
+#     print(f"An error occurred: {e}")
 
 
 
-import csv
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host="",
-    user="root",
-    password="",
-    database="s"
-)
-
-# Open the CSV file and read content
-with open('dnd.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        mobail_no = row[0]
-        first_three_letters_mobail_no = mobail_no[:3] if len(mobail_no) >= 3 else mobail_no 
-
-        query = "SELECT COUNT(*) FROM `insert_new` WHERE `mobail_no` = %s"
-        mycursor = mydb.cursor()
-        mycursor.execute(query, (mobail_no,))
-        result = mycursor.fetchone()
-
-        if result[0] == 0:
-            F1 = '1'
-            F4 = '4'
-            F5 = '5'
-            F3 = '6'
-
-            # Insert data into the database
-            insert_query = "INSERT INTO `insert_new` (`F1`, `mobail_no`, `F3`, `F4`,`prefix`, `F5`) VALUES (%s, %s, %s, %s, %s, %s)"
-            insert = (F1, mobail_no,  F3, F4, first_three_letters_mobail_no, F5)
-            mycursor.execute(insert_query, insert)
-            mydb.commit()
-
-# Close the database connection
-mydb.close()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import csv
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#     host="",
+#     user="root",
+#     password="",
+#     database="s"
+# )
+
+# # Open the CSV file and read content
+# with open('dndfinal.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     for row in csv_reader:
+#         mobail_no = row[0]
+#         first_three_letters = mobail_no[:3] if len(mobail_no) >= 3 else mobail_no 
+
+#         query = "SELECT COUNT(*) FROM `insert_new` WHERE `mobail_no` = %s"
+#         mycursor = mydb.cursor()
+#         mycursor.execute(query, (mobail_no,))
+#         result = mycursor.fetchone()
+
+#         if result[0] == 0:
+#             F1 = '1'
+#             F4 = '4'
+#             F5 = '5'
+#             F3 = '6'
+
+#             # Insert data into the database
+#             insert_query = "INSERT INTO `insert_new` (`F1`, `mobail_no`, `F3`, `F4`,`prefix`, `F5`) VALUES (%s, %s, %s, %s, %s, %s)"
+#             insert = (F1, mobail_no,  F3, F4, first_three_letters, F5)
+#             mycursor.execute(insert_query, insert)
+#             mydb.commit()
+
+# # Close the database connection
+# mydb.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import csv
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/s'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+
+# class Insert_new(db.Model):
+#     __tablename__ = 'insert_new'
+#     id = db.Column(db.Integer, primary_key=True)
+#     F1 = db.Column(db.String(100), nullable=False)
+#     mobail_no = db.Column(db.String(100), nullable=False)
+#     F3= db.Column(db.String(100), nullable=False)
+#     F4 = db.Column(db.String(100), nullable=False)
+#     prefix = db.Column(db.String(200), nullable=False)
+#     F5 = db.Column(db.String(200), nullable=False)
+
+# # Read the CSV file and insert data into the database
+# def read_csv_and_insert():
+#     with open('dndfinal.csv') as csv_file:
+#         csv_reader = csv.reader(csv_file, delimiter=',')
+        
+#         for row in csv_reader:
+#             mobail_no = row[0]
+#             first_three_letters_mobail_no = mobail_no[:3] if len(mobail_no) >= 3 else mobail_no
+
+#             existing_entry = Insert_new.query.filter_by(mobail_no=mobail_no).first()
+
+#             if not existing_entry:
+#                 F1 = '1'
+#                 F4 = '4'
+#                 F5 = '5'
+#                 F3 = '6'
+
+#                 new_data = Insert_new(F1=F1, mobail_no=mobail_no, F3=F3, F4=F4, 
+#                                       prefix=first_three_letters_mobail_no, F5=F5)
+#                 db.session.add(new_data)
+#                 db.session.commit()
+
+
+# with app.app_context():
+#     read_csv_and_insert()
+
+
+
+
+
+
+
+
+
+
+
+
+import pandas as pd
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configure the database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/s'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class InsertNew(db.Model):
+    __tablename__ = 'insert_new'
+    id = db.Column(db.Integer, primary_key=True)
+    F1 = db.Column(db.String(100), nullable=False)
+    mobail_no = db.Column(db.String(100), nullable=False)
+    F3 = db.Column(db.String(100), nullable=False)
+    F4 = db.Column(db.String(100), nullable=False)
+    prefix = db.Column(db.String(200), nullable=False)
+    F5 = db.Column(db.String(200), nullable=False)
+
+def read_excel_and_insert():
+    try:
+        excel_data = pd.read_excel('dndrai.xlsx', header=None)
+        
+        for index, row in excel_data.iterrows():
+            mobail_no = str(row[0])
+            first_three_letters_mobail_no = mobail_no[:3] if len(mobail_no) >= 3 else mobail_no
+
+            existing_entry = InsertNew.query.filter_by(mobail_no=mobail_no).first()
+
+            if not existing_entry:
+                F1 = '1'
+                F4 = '4'
+                F5 = '5'
+                F3 = '6'
+
+                new_data = InsertNew(F1=F1,mobail_no=mobail_no,F3=F3,F4=F4,
+                                     prefix=first_three_letters_mobail_no,F5=F5
+                )
+                db.session.add(new_data)
+                db.session.commit()
+
+        return "Data inserted successfully!"
+
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+
+with app.app_context():
+    result = read_excel_and_insert()
+    print(result) 
