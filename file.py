636f5c3bc8fb4cba907d7f5e6a08c4d03bf8cf13
sundram <sundrammaurya1996@gmail.com>
2024-01-16 17:15:08 +0530

    new change

--- file.py
+++ file.py
@@ -162,62 +162,6 @@
 
 
 
-
-# import pandas as pd
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-
-# # Configure the database connection
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/s'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-
-# class Sdm(db.Model):
-#     __tablename__ = 'sdm'
-#     id = db.Column(db.Integer, primary_key=True)
-#     F1 = db.Column(db.String(100), nullable=False)
-#     mobail_no = db.Column(db.String(100), nullable=False)
-#     F3 = db.Column(db.String(100), nullable=False)
-#     F4 = db.Column(db.String(100), nullable=False)
-#     prefix = db.Column(db.String(200), nullable=False)
-#     F5 = db.Column(db.String(200), nullable=False)
-
-# def read_excel_and_insert():
-#     excel_data = pd.read_excel('dndrai.xlsx', header=None)
-        
-#     for index, row in excel_data.iterrows():
-#         mobail_no = str(row[0])
-#         first_three_letters_mobail_no = mobail_no[:3] if len(mobail_no) >= 3 else mobail_no
-
-#         existing_entry = Sdm.query.filter_by(mobail_no=mobail_no).first()
-
-#         if not existing_entry:
-#             F1 = '1'
-#             F3 = '6'
-#             F4 = '4'
-#             F5 = '5'
-
-#             new_data = Sdm(F1=F1,mobail_no=mobail_no,F3=F3,F4=F4,
-#                                      prefix=first_three_letters_mobail_no,F5=F5
-#                 )
-#             db.session.add(new_data)
-#             db.session.commit()
-
-# with app.app_context():
-#     result = read_excel_and_insert()
-#     print(result) 
-
-
-
-
-
-
-
-
-
-
 # import pandas as pd
 # from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
@@ -327,84 +271,89 @@
 
 
 
-import datetime
-import openpyxl
-import pandas as pd
-import pymysql
-from concurrent.futures import ThreadPoolExecutor
+# import datetime
+# import openpyxl
+# import pandas as pd
+# import pymysql
+# from concurrent.futures import ThreadPoolExecutor
 
-# Connect to the MySQL database
-try:
-    connection = pymysql.connect(host='localhost', user='root', password='', database='s',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    print("DB connection successful")
-except:
-    print("DB connection failed")
+# # Connect to the MySQL database
+# try:
+#     connection = pymysql.connect(host='localhost', user='root', password='', database='s',
+#                                  cursorclass=pymysql.cursors.DictCursor)
+#     print("DB connection successful")
+# except:
+#     print("DB connection failed")
 
-def data_insert(data):
-    duplicate_data = set()
-    duplicate = 0 
+# def data_insert(data):
+#     duplicate_data = set()
+#     duplicate = 0 
 
-    try:
-        with connection.cursor() as cursor:
-            select_query = "SELECT F2 FROM insert_new"
-            cursor.execute(select_query)
-            sdm = cursor.fetchall()
+#     try:
+#         with connection.cursor() as cursor:
+#             select_query = "SELECT F2 FROM insert_new"
+#             cursor.execute(select_query)
+#             sdm = cursor.fetchall()
 
-            for row in sdm:
-                duplicate_data.add(row['F2'])  
-    except :
-        print("Duplicate data error")
+#             for row in sdm:
+#                 duplicate_data.add(row['F2'])  
+#     except :
+#         print("Duplicate data error")
 
-    list_data = []
+#     list_data = []
 
-    for _, row in data.iterrows():
-        mobail_no = str(row[0])
-        three_letters_mobail_no = mobail_no[:3] if len(mobail_no) >= 3 else mobail_no
+#     for _, row in data.iterrows():
+#         mobail_no = str(row[0])
+#         three_letters_mobail_no = mobail_no[:3] if len(mobail_no) >= 3 else mobail_no
 
-        if mobail_no not in duplicate_data:
-            entry = {
-                'F1': '1',
-                'F2': mobail_no,
-                'F3': '6',
-                'F4': '4',
-                'prefix': three_letters_mobail_no,
-                'F5': '5'
-            }
-            list_data.append(entry)
-        else:
-            duplicate += 1
+#         if mobail_no not in duplicate_data:
+#             entry = {
+#                 'F1': '1',
+#                 'F2': mobail_no,
+#                 'F3': '6',
+#                 'F4': '4',
+#                 'prefix': three_letters_mobail_no,
+#                 'F5': '5'
+#             }
+#             list_data.append(entry)
+#         else:
+#             duplicate += 1
 
 
-    try:
-        if list_data:
-            with connection.cursor() as cursor:
-                insert_query = "INSERT INTO insert_new (F1, F2, F3, F4, prefix, F5) VALUES (%(F1)s, %(F2)s, %(F3)s, %(F4)s, %(prefix)s, %(F5)s)"
-                cursor.executemany(insert_query, list_data)
-                connection.commit()
-                print("Data inserted successfully")
+#     try:
+#         if list_data:
+#             with connection.cursor() as cursor:
+#                 insert_query = "INSERT INTO insert_new (F1, F2, F3, F4, prefix, F5) VALUES (%(F1)s, %(F2)s, %(F3)s, %(F4)s, %(prefix)s, %(F5)s)"
+#                 cursor.executemany(insert_query, list_data)
+#                 connection.commit()
+#                 print("Data inserted successfully")
 
-                for data in list_data:
-                    print(data)
+#                 for data in list_data:
+#                     print(data)
                 
-    except:
-        print("Error Insert ")
+#     except:
+#         print("Error Insert ")
 
-    print(f"Total duplicate found: {duplicate}")
+#     print(f"Total duplicate found: {duplicate}")
 
-def read_csv_insert():
-    current_date = datetime.datetime.now().strftime("%Y_%m_%d")
-    file_path = f'MergedDndNoOfFiveServer_{current_date}.csv'
-    csv_data = pd.read_csv(file_path, header=None)
+# def read_csv_insert():
+#     current_date = datetime.datetime.now().strftime("%Y_%m_%d")
+#     file_path = f'MergedDndNoOfFiveServer_{current_date}.csv'
+#     csv_data = pd.read_csv(file_path, header=None)
     
     
-    print(file_path)
+#     print(file_path)
     
-    data_size = 100000  
-    data = [csv_data[i:i + data_size] for i in range(0, csv_data.shape[0], data_size)]
+#     data_size = 100000  
+#     data = [csv_data[i:i + data_size] for i in range(0, csv_data.shape[0], data_size)]
 
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        executor.map(data_insert, data)
+#     with ThreadPoolExecutor(max_workers=1) as executor:
+#         executor.map(data_insert, data)
 
-read_csv_insert()
-connection.close()
+# read_csv_insert()
+# connection.close()
+
+
+
+
+
